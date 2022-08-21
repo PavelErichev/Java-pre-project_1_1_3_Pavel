f0276893f33346dfb95c406d8cd4ea94a72291d0
PavelErichev <dustfud@yandex.ru>
2022-08-21 22:53:04 +0300

    Задача 1.1.3 испарвлен пароль на root

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mysql";
-    private static final String USERNAME = "dustfud";
-    private static final String PASSWORD = "dustfud";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
     public static Connection getConnection() {
         Connection connection = null;
 
